55605d0dea9d78dbecdf87605410e9dbb4ecb8f7
Tomek <rzepka.tomek@gmail.com>
2024-01-20 00:27:30 +0100

    refactor CourseService

--- src/main/java/com/example/elearningplatform/user/UserMapper.java
+++ src/main/java/com/example/elearningplatform/user/UserMapper.java
@@ -1,10 +1,14 @@
 package com.example.elearningplatform.user;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor
 public class UserMapper {
 
+    private final PasswordEncoder encoder;
     public UserDto map(UserEntity entity) {
         return new UserDto(entity.getLogin(),
                 entity.getPassword(),
@@ -12,9 +16,9 @@ public class UserMapper {
                 entity.getRole());
     }
 
-    public UserDto mapExcludedPassword(UserEntity entity) {
+    public UserDto mapEncodePassword(UserEntity entity) {
         return new UserDto(entity.getLogin(),
-                "????",
+                encoder.encode(entity.getPassword()),
                 entity.getEmail(),
                 entity.getRole());
     }
